ed4a8674c95ae0a96c3bc3dabfdbf1270441bf34
yqgz <yqgz_yy@126.com>
2021-06-10 17:31:28 +0800

    使用公共页面 添加bootstrap table

--- RTdemo/views.py
+++ RTdemo/views.py
@@ -6,7 +6,7 @@ import os
 #获取参数列表
 def para_in(request):
     cursor = connection.cursor()
-    cursor.execute("select * from test_p")
+    cursor.execute("select p.para_name,p.display_name,p.unit,c.chapter_name from test_p p,chapter c where p.chapter_id=c.idchapter")
     rows = cursor.fetchall()
     data={"paras":rows}
     return render(request,"demo_page.html",context=data)
