589ba2afae71e9e7e944ebaf865d3cd5c3fb480b
yqgz <yqgz_yy@126.com>
2021-06-17 18:16:33 +0800

    修改demo页样式

--- RTdemo/JS/demo_page.js
+++ RTdemo/JS/demo_page.js
@@ -1,68 +1,80 @@
-$(function() {
-    let $table = $('#table');
-    //let $button = $('#button');
-    let $getTableData = $('#getTableData');
+function editpara(obj) {
+    var tr = $(obj).parent().parent();
+    print(tr);
+   /* var chapter = tr.children("td#title").text();
+    var parameter = tr.children("td#body").text();
+    var unit = tr.children("td#body").text();
+    $('#chapter').val(chapter);
+    $('#parameter').val(parameter);
+    $('#unit').val(unit);*/
+    $('#exampleModal').modal('show')
 
-    /*$button.click(function() {
-        $table.bootstrapTable('insertRow', {
-            index: 0,
-            row: {
-                id: '',
-                name: '',
-                price: ''
-            }
-        });
-    });*/
-
-    $table.bootstrapTable({
-        url: 'data2.json',
-        toolbar: '#toolbar',
-        clickEdit: true,
-        showToggle: true,
-        pagination: true,       //显示分页条
-        showColumns: true,
-        showPaginationSwitch: true,     //显示切换分页按钮
-        showRefresh: true,      //显示刷新按钮
-        //clickToSelect: true,  //点击row选中radio或CheckBox
-        columns: [{
-            checkbox: true
-        }, {
-            field: 'id',
-            title: 'Item ID'
-        }, {
-            field: 'name',
-            title: 'Item Name'
-        }, {
-            field: 'price',
-            title: 'Item Price'
-        }, ],
-        /**
-         * @param {点击列的 field 名称} field
-         * @param {点击列的 value 值} value
-         * @param {点击列的整行数据} row
-         * @param {td 元素} $element
-         */
-        onClickCell: function(field, value, row, $element) {
-            $element.attr('contenteditable', true);
-            $element.blur(function() {
-                let index = $element.parent().data('index');
-                let tdValue = $element.html();
-
-                saveData(index, field, tdValue);
-            })
-        }
-    });
-
-    $getTableData.click(function() {
-        alert(JSON.stringify($table.bootstrapTable('getData')));
-    });
-
-    function saveData(index, field, value) {
-        $table.bootstrapTable('updateCell', {
-            index: index,       //行索引
-            field: field,       //列名
-            value: value        //cell值
-        })
-    }
-
-});
+};
+// $(function() {
+//     let $table = $('#table');
+//     //let $button = $('#button');
+//     let $getTableData = $('#getTableData');
+//
+//     /*$button.click(function() {
+//         $table.bootstrapTable('insertRow', {
+//             index: 0,
+//             row: {
+//                 id: '',
+//                 name: '',
+//                 price: ''
+//             }
+//         });
+//     });*/
+//
+//     $table.bootstrapTable({
+//         url: 'data2.json',
+//         toolbar: '#toolbar',
+//         clickEdit: true,
+//         showToggle: true,
+//         pagination: true,       //显示分页条
+//         showColumns: true,
+//         showPaginationSwitch: true,     //显示切换分页按钮
+//         showRefresh: true,      //显示刷新按钮
+//         //clickToSelect: true,  //点击row选中radio或CheckBox
+//         columns: [{
+//             checkbox: true
+//         }, {
+//             field: 'id',
+//             title: 'Item ID'
+//         }, {
+//             field: 'name',
+//             title: 'Item Name'
+//         }, {
+//             field: 'price',
+//             title: 'Item Price'
+//         }, ],
+//         /**
+//          * @param {点击列的 field 名称} field
+//          * @param {点击列的 value 值} value
+//          * @param {点击列的整行数据} row
+//          * @param {td 元素} $element
+//          */
+//         onClickCell: function(field, value, row, $element) {
+//             $element.attr('contenteditable', true);
+//             $element.blur(function() {
+//                 let index = $element.parent().data('index');
+//                 let tdValue = $element.html();
+//
+//                 saveData(index, field, tdValue);
+//             })
+//         }
+//     });
+//
+//     $getTableData.click(function() {
+//         alert(JSON.stringify($table.bootstrapTable('getData')));
+//     });
+//
+//     function saveData(index, field, value) {
+//         $table.bootstrapTable('updateCell', {
+//             index: index,       //行索引
+//             field: field,       //列名
+//             value: value        //cell值
+//         })
+//     }
+//
+// });
